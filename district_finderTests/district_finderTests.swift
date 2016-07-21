f9bf1e4458bf58e500a50b1d826709589ebd397e
jacobdlewis <jacobdlewis@gmail.com>
2016-07-20 19:04:40 -0500

    Initial Commit

---
+++ district_finderTests/district_finderTests.swift
@@ -0,0 +1,36 @@
+//
+//  district_finderTests.swift
+//  district_finderTests
+//
+//  Created by Jacob Lewis on 7/20/16.
+//  Copyright © 2016 nashville_beginners. All rights reserved.
+//
+
+import XCTest
+@testable import district_finder
+
+class district_finderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
